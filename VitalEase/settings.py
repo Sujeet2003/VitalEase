4293ca8b04f78757a5e7372611a2d53a317ac486
Sujeet2003 <sujeetsmpatel@gmail.com>
2024-07-11 14:55:42 +0530

    changes

--- VitalEase/settings.py
+++ VitalEase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--)*5a0nu_&7u9o1zke_sx$8%#fe*#$vj))dwkq2*)od&wy5p)_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '*']
 
 
 # Application definition
@@ -114,9 +114,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR/ 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
