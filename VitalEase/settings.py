7a346763081bdaa68d11b721b943b338640d5fd0
Sujeet2003 <sujeetsmpatel@gmail.com>
2024-07-11 15:42:48 +0530

    changes

--- VitalEase/settings.py
+++ VitalEase/settings.py
@@ -115,9 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-STATIC_URL = 'static/'
-STSTICFILES_DIRS = [os.path.join(BASE_DIR/ "static")]
-STATIC_ROOT = os.path.join(BASE_DIR/ "staticfiles")
+STATIC_URL = '/static/'
+STSTICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
