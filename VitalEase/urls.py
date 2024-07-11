136e55a9b10404ba914be0a550cf2fc17bc60be0
Sujeet2003 <sujeetsmpatel@gmail.com>
2024-07-11 15:13:59 +0530

    changes

--- VitalEase/urls.py
+++ VitalEase/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     path('Emergency-Contact/', views.contact),
     path('Caregiver-Page/', views.caregiver),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
